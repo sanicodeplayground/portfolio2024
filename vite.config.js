43edc508a8ff5510de9952a85680f11719757fae
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-01-14 11:46:17 +0000

    404 created

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio2024/",
+  base: process.env.NODE_ENV === "production" ? "/portfolio2024/" : "/",
   plugins: [react()],
 });
