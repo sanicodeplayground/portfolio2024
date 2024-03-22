58d91d28d60cec65ac431382c9856a3c7bdcc9e1
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-03-22 17:42:55 +0000

    tailwind reusable component on menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
 
   const toggleMenu = () => {
     setIsMenuOpen((prevState) => !prevState); // Toggle based on previous state
   };
+
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [location]);
 
   return (
     <header>
@@ -70,43 +75,27 @@ function Header() {
           >
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
               <li>
-                <Link
-                  to="/"
-                  className="ease-in duration-700 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  aria-current="page"
-                >
+                <Link to="/" className="menu" aria-current="page">
                   Home
                 </Link>
               </li>
               <li>
-                <Link
-                  to="/about"
-                  className="ease-in duration-700 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
+                <Link to="/about" className="menu">
                   About
                 </Link>
               </li>
               <li>
-                <Link
-                  to="/projects"
-                  className="ease-in duration-700 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
+                <Link to="/projects" className="menu">
                   Projects
                 </Link>
               </li>
               <li>
-                <Link
-                  to="/blog"
-                  className="ease-in duration-700 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
+                <Link to="/blog" className="menu">
                   Blog
                 </Link>
               </li>
               <li>
-                <Link
-                  to="/contact"
-                  className="ease-in duration-700 block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
+                <Link to="/contact" className="menu">
                   Contact
                 </Link>
               </li>
