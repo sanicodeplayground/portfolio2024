8bf982172f4dde0b713915fcdaca5dc0d3868884
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-04-30 12:44:34 +0000

    array for portfolio

---
+++ src/components/ProjectsData.jsx
@@ -0,0 +1,44 @@
+
+const projects = [
+  {
+    id: 'connou',
+    image: './images/portfolio/portfolioConnou.jpg',
+    title: 'Connou',
+    previewURL: 'https://connou.app',
+    description: ['Junior Front End Developer', 'Tailwind v3.4', 'React', 'Next.js']
+  },
+  {
+    id: 'bank-dashboard',
+    image: './images/portfolio/bank-app.jpg',
+    title: 'Bank Dashboard',
+    previewURL: 'https://sanicodeplayground.github.io/bank-dashboard',  
+    githubURL: 'https://github.com/sanicodeplayground/bank-dashboard',
+    description: ['Vanila JavaScript', 'Array methods', 'DOM manipulation']
+  },
+  {
+    id: 'veggie',
+    image: './images/portfolio/portfolioVerde.jpg',
+    title: 'Veggie Recipes',
+    previewURL: 'https://tailwindcss-shop.vercel.app/',  
+    githubURL: 'https://github.com/sanicodeplayground/tailwindcss',
+    description: ['Vercel deployment', 'Tailwind v3.4', 'JavaScript']
+  },
+  {
+    id: 'dice-game',
+    image: './images/portfolio/portfolioGameDice.jpg',
+    title: 'Dice Game',
+    previewURL: 'https://sanicodeplayground.github.io/js-dice-game/',  
+    githubURL: 'https://github.com/sanicodeplayground/js-dice-game',
+    description: ['Vanilla JavaScript', 'Game logic', 'Array and functions']
+  },
+  {
+    id: 'newsletter-validation',
+    image: './images/portfolio/portfolioNewsletterValidation.jpg',
+    title: 'Newsletter Validation',
+    previewURL: 'https://react-newsletter-sign-up-rho.vercel.app/',  
+    githubURL: 'https://github.com/sanicodeplayground/react-newsletter-sign-up-form',
+    description: ['React state hook', 'Form input validation', 'Error handling']
+  }
+]
+
+export default projects;
