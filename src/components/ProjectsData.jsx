f7fadfa8adf4483571eb5c35c612fa4efddc6d0f
sanicodeplayground <106916427+sanicodeplayground@users.noreply.github.com>
2024-05-07 10:56:03 +0000

    reorder portfolio items

--- src/components/ProjectsData.jsx
+++ src/components/ProjectsData.jsx
@@ -1,52 +1,64 @@
-
 const projects = [
   {
-    id: 'connou',
-    image: './images/portfolio/portfolioConnou.jpg',
-    title: 'Connou',
-    previewURL: 'https://connou.app',
-    description: ['Junior Front End Developer', 'Tailwind v3.4', 'React', 'Next.js']
+    id: "connou",
+    image: "./images/portfolio/portfolioConnou.jpg",
+    title: "Connou",
+    previewURL: "https://connou.app",
+    description: ["Frontend Developer", "React", "Next.js", "Tailwind CSS"],
   },
   {
-    id: 'bank-dashboard',
-    image: './images/portfolio/bank-app.jpg',
-    title: 'Bank Dashboard',
-    previewURL: 'https://sanicodeplayground.github.io/bank-dashboard',  
-    githubURL: 'https://github.com/sanicodeplayground/bank-dashboard',
-    description: ['Vanila JavaScript', 'Array methods', 'DOM manipulation']
+    id: "facts",
+    image: "./images/portfolio/portfolioDailyDiscoveries.png",
+    title: "Daily Discoveries",
+    previewURL: "https://dailydiscoveries.netlify.app/",
+    githubURL: "https://github.com/sanicodeplayground/daily-discoveries",
+    description: [
+      "React",
+      "Props",
+      "State management",
+      "DOM manipulation",
+      "API Supabase database",
+    ],
   },
   {
-    id: 'facts',
-    image: './images/portfolio/portfolioDailyDiscoveries.png',
-    title: 'Daily Discoveries',
-    previewURL: 'https://dailydiscoveries.netlify.app/',  
-    githubURL: 'https://github.com/sanicodeplayground/daily-discoveries',
-    description: ['React', 'Props', 'State management', 'DOM manipulation', 'API Supabase']
+    id: "dice-game",
+    image: "./images/portfolio/portfolioGameDice.jpg",
+    title: "Dice Game",
+    previewURL: "https://sanicodeplayground.github.io/js-dice-game/",
+    githubURL: "https://github.com/sanicodeplayground/js-dice-game",
+    description: ["Vanilla JavaScript", "Game logic", "Array and functions"],
   },
   {
-    id: 'veggie',
-    image: './images/portfolio/portfolioVerde.jpg',
-    title: 'Veggie Recipes',
-    previewURL: 'https://tailwindcss-shop.vercel.app/',  
-    githubURL: 'https://github.com/sanicodeplayground/tailwindcss',
-    description: ['Vercel deployment', 'Tailwind v3.4', 'JavaScript']
+    id: "bank-dashboard",
+    image: "./images/portfolio/bank-app.jpg",
+    title: "Bank Dashboard",
+    previewURL: "https://sanicodeplayground.github.io/bank-dashboard",
+    githubURL: "https://github.com/sanicodeplayground/bank-dashboard",
+    description: ["Vanila JavaScript", "Array methods", "DOM manipulation"],
   },
+
   {
-    id: 'dice-game',
-    image: './images/portfolio/portfolioGameDice.jpg',
-    title: 'Dice Game',
-    previewURL: 'https://sanicodeplayground.github.io/js-dice-game/',  
-    githubURL: 'https://github.com/sanicodeplayground/js-dice-game',
-    description: ['Vanilla JavaScript', 'Game logic', 'Array and functions']
+    id: "veggie",
+    image: "./images/portfolio/portfolioVerde.jpg",
+    title: "Veggie Recipes",
+    previewURL: "https://tailwindcss-shop.vercel.app/",
+    githubURL: "https://github.com/sanicodeplayground/tailwindcss",
+    description: ["Vercel deployment", "Tailwind v3.4", "JavaScript"],
   },
+
   {
-    id: 'newsletter-validation',
-    image: './images/portfolio/portfolioNewsletterValidation.jpg',
-    title: 'Newsletter Validation',
-    previewURL: 'https://react-newsletter-sign-up-rho.vercel.app/',  
-    githubURL: 'https://github.com/sanicodeplayground/react-newsletter-sign-up-form',
-    description: ['React state hook', 'Form input validation', 'Error handling']
-  }
-]
+    id: "newsletter-validation",
+    image: "./images/portfolio/portfolioNewsletterValidation.jpg",
+    title: "Newsletter Validation",
+    previewURL: "https://react-newsletter-sign-up-rho.vercel.app/",
+    githubURL:
+      "https://github.com/sanicodeplayground/react-newsletter-sign-up-form",
+    description: [
+      "React state hook",
+      "Form input validation",
+      "Error handling",
+    ],
+  },
+];
 
 export default projects;
